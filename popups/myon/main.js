158954da427a3d4ad62af6c56c74b24f9819112d
Astrol99 <dngames2006@gmail.com>
2020-05-19 12:14:06 -0500

    Added button change onclick and slider enable state onclick

--- popups/myon/main.js
+++ popups/myon/main.js
@@ -1,9 +1,11 @@
-// This looks kinda messy
 const readTimeSpan = document.getElementById("readTimeSpan");
 const readTimeSlider = document.getElementById("readTimeSlider");
 
 const delayTimeSpan = document.getElementById("delayTimeSpan");
 const delayTimeSlider = document.getElementById("delayTimeSlider");
+
+const button = document.getElementById("mainBtn");
+let start = false;
 
 dynamicSliderValue(readTimeSlider, readTimeSpan, "Minutes");
 dynamicSliderValue(delayTimeSlider, delayTimeSpan, "Seconds");
@@ -14,3 +16,21 @@ function dynamicSliderValue(slider, span, type) {
         span.innerHTML = `${slider.value} ${type}`; 
     };
 }
+
+button.addEventListener("click", () => {
+    start = !start;
+
+    // Disable/Enable sliders according to start variable
+    readTimeSlider.disabled = start;
+    delayTimeSlider.disabled = start;
+
+    if (start) {
+        // Change button style to bootstrap danger btn and text to "stop"
+        button.innerHTML = "Stop Auto Read";
+        button.className = "btn btn-danger btn-lg";
+    } else if (!start) {
+        // Original Style
+        button.innerHTML = "Start Auto Read";
+        button.className = "btn btn-outline-primary btn-lg"
+    }
+});
